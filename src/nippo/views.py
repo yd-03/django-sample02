7bb3dbf88457d6a9845cab150698a7fe7fa26bae
yd-03 <3nu4ydye57@gmail.com>
2024-08-28 09:01:07 +0900

    chore: 各コードのコメントを追加

--- src/nippo/views.py
+++ src/nippo/views.py
@@ -4,25 +4,34 @@ from .forms import NippoFormClass
 
 
 def nippoListView(request):
+    """日報の一覧を表示するビュー関数"""
     template_name = "nippo/nippo-list.html"
     ctx = {}
+    # モデルの全てのオブジェクトをDBから取得
     qs = NippoModel.objects.all()
+    # テンプレートに渡すコンテキストにオブジェクトリストを追加
     ctx["object_list"] = qs
+    # テンプレートをレンダリングし、コンテキストを渡してHTMLを生成
     return render(request, template_name, ctx)
 
 
 def nippoDetailView(request, pk):
+    """特定の日報の詳細を表示するビュー関数"""
     template_name = "nippo/nippo-detail.html"
     ctx = {}
+    # モデルの特定のオブジェクトをDBから取得
     q = NippoModel.objects.get(pk=pk)
     ctx["object"] = q
     return render(request, template_name, ctx)
 
 
 def nippoCreateView(request):
+    """新しい日報を作成するビュー関数"""
     template_name = "nippo/nippo-form.html"
+    # フォームクラスを使用してフォームを作成（初期リクエスト時は空のフォーム）
     form = NippoFormClass(request.POST or None)
     ctx = {"form": form}
+    # フォームが正しいかどうかをチェック
     if form.is_valid():
         title = form.cleaned_data["title"]
         content = form.cleaned_data["content"]
@@ -32,9 +41,12 @@ def nippoCreateView(request):
 
 
 def nippoUpdateFormView(request, pk):
+    """既存の日報を編集するビュー関数"""
     template_name = "nippo/nippo-form.html"
     obj = NippoModel.objects.get(pk=pk)
+    # フォームクラスを使用してフォームを作成（初期値として、既存のオブジェクトの値をフォームにセット）
     initial_values = {"title": obj.title, "content": obj.content}
+    # POSTリクエスト時はリクエストデータをフォームにセット、なければ初期値をセット
     form = NippoFormClass(request.POST or initial_values)
     ctx = {"form": form}
     ctx["object"] = obj
